118a2199e1509ee5148149814a5bf6b1205e5872
michelss2018 <michellss2018@gmail.com>
2021-01-13 19:30:44 -0300

    Primeiro commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+var nome = "Michel Souza Santana"
+var nomeHTML = document.getElementById("nome")
+
+var cargo = "Web Developer"
+var cargoHTML = document.getElementById("cargo")
+
+var sobre = document.getElementById("textoSobre")
+var projetos = document.getElementById("textoProjetos")
+
+function colocaNomeCargo(){
+    nomeHTML.innerHTML = nome
+    cargoHTML.innerHTML = cargo
+}
+
+colocaNomeCargo()
+
+/**************************/
+
+function onProjetos(){    
+    sobre.style.display = "none"
+    projetos.style.display = "block"
+}
+
+function onSobre(){    
+    sobre.style.display = "block"
+    projetos.style.display = "none"
+}
+
+
