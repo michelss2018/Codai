7238a53676ccf458e37294c98e8330fc84fe1ac1
michelss2018 <michellss2018@gmail.com>
2021-01-31 14:43:34 -0300

    modificações no lyout

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var nome = "Michel Souza"
 var nomeHTML = document.getElementById("nome")
 
-var cargo = "Web Developer"
+var cargo = "Dev Full Stack"
 var cargoHTML = document.getElementById("cargo")
 
 var sobre = document.getElementById("textoSobre")
@@ -38,3 +38,11 @@ function trocaImgProjetos(){
     img.innerHTML = '<img src="/img/michel1.gif" alt="Foto de Perfil" class="card-image mx-auto d-block">'
 }
 
+/****************************/
+function openADS(){
+    window.open("curriculoADS.html")
+}
+
+function openADM(){
+    window.open("curriculoADM.html")
+}
